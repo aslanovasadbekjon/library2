8a683aa1aae46cf06fe3145357eb9f3408457534
aslanovasadbekjon <87481245+aslanovasadbekjon@users.noreply.github.com>
2021-08-03 10:22:58 +0500

    9-dars yarmi
    categoryda xato bor

--- librarySite/libnews/views.py
+++ librarySite/libnews/views.py
@@ -6,17 +6,17 @@ from .models import Obyekt, Bolim
 
 def index(request):
 	libnews = Obyekt.objects.all()
-	bolimlar = Bolim.objects.all()
+	# bolimlar = Bolim.objects.all()
 	context = {
 	'libnews':libnews,
 	'title':"Kitoblar olami",
-	'bolimlar': bolimlar,
+	# 'bolimlar': bolimlar,
 	
 	}
 	return render(request,template_name= 'libnews/index.html',context=context)
 
 def get_bolim(request,bolim_id):
 	libnews = Obyekt.objects.filter(bolim_id=bolim_id)
-	bolimlar=Bolim.objects.all()
+	# bolimlar=Bolim.objects.all()
 	bolim = Bolim.objects.get(pk=bolim_id)
 	return render(request,'libnews/bolim.html',{'libnews':libnews, 'bolimlar':bolimlar,'bolim':bolim})
